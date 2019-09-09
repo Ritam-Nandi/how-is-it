540f019d53cdcbae68828d93b48f084b98a8b666
Ritam-Nandi <ritam.nandi.98@gmail.com>
2019-09-09 23:02:57 +0530

    test

--- src/component/Product.js
+++ src/component/Product.js
@@ -4,7 +4,7 @@ export default class Product extends Component {
   render() {
     return (
       <div>
-        <h3>Hello from Product ~ All of them</h3>
+        <h3>Hello CP</h3>
       </div>
     );
   }
